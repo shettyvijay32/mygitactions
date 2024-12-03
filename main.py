51bebb43af8a11dca6ccc152deae4576fd6390bc
shettyvijay32 <144682707+shettyvijay32@users.noreply.github.com>
2024-12-03 12:34:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def hello():
     def bye():
         print("bye")
+
     bye()  # Call the bye() function inside hello()
     return "Hello from hello()"
 
-# Call hello() and print its return value
-print(hello())
+
