3bf7e76a83f62205190856e22c8c693bc9b76f17
shettyvijay32 <144682707+shettyvijay32@users.noreply.github.com>
2024-12-16 12:49:08 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,13 @@
 # app.py
-# This is a test commit
+import unittest
+
 def add(a, b):
     return a + b
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
+class TestAddFunction(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(1, -1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
